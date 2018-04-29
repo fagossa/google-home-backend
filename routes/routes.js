9b200c2d22cf22fd8d4dd608db5080eb84a053ab
Fabian Gutierrez <fagossa@gmail.com>
2018-04-29 16:45:01 +0200

    refactoring

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,7 @@
 var express = require('express');
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
 
 var router = express.Router();
 
@@ -8,14 +11,34 @@ router.get('/', function(req, res) {
   return
 });
 
-router.post('/values', function(req, res) {
+router.post('/callback', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  var val = req.body.value;
 
-  if (val === undefined || val === "") {
-    res.send(JSON.stringify({status: "error", value: "Value undefined"}));
-    return
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
   }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function handleWeather(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('weather', handleWeather);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+
 });
 
 module.exports = router;
